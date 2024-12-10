cbe58fda689c58183118524e2ebceaccf98ad505
DARKZXxl <gabrielfigueira88@gmail.com>
2024-12-10 09:05:34 -0300

    Initial commit

---
+++ inicio.js
@@ -0,0 +1,7 @@
+const sozinho = document.getElementById('sozinho').addEventListener('click', () => {
+    location.href = 'sozinho.html'
+})
+
+const comAmigo = document.getElementById('comAmigo').addEventListener('click', () => {
+    location.href = 'index.html'
+})
